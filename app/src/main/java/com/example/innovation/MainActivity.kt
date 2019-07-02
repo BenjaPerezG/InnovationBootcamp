e6ea3d1975c56ed51a8a663df74ca4230498e2ec
BenjaPerezG <benjaperezg@gmail.com>
2019-07-02 11:44:30 -0400

    main activity with button and timer

--- app/src/main/java/com/example/innovation/MainActivity.kt
+++ app/src/main/java/com/example/innovation/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.innovation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        object : CountDownTimer(30000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                timerText.text = "seconds remaining: ${millisUntilFinished / 1000}"
+            }
+
+            override fun onFinish() {
+                timerText.text = "done!"
+            }
+        }.start()
+
     }
 }
