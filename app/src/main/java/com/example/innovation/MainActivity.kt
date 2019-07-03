8a3114982a61c336d6d39e25c3b70c67d153c157
BenjaPerezG <benjaperezg@gmail.com>
2019-07-03 15:40:07 -0400

    Counter format fixed

--- app/src/main/java/com/example/innovation/MainActivity.kt
+++ app/src/main/java/com/example/innovation/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.innovation
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -8,22 +10,26 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
+   /* val PRIVATE_MODE = 0
+    val PREFERENCE_NAME: String = getString(R.string.settings_preferences)
+    val PREFERENCE_VALUE: String = getString(R.string.timer_preferences)
+    val shared_preference: SharedPreferences = getSharedPreferences(PREFERENCE_NAME, PRIVATE_MODE)*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        object : CountDownTimer(30000, 1000) {
+        /*val timer_start: Long = shared_preference.getLong(PREFERENCE_VALUE, 30000)*/
+        val counter = object : CountDownTimer(300000, 1000) {
 
             override fun onTick(millisUntilFinished: Long) {
-                timerText.text = "seconds remaining: ${millisUntilFinished / 1000}"
+                timerText.text = "seconds remaining: ${(millisUntilFinished / 1000)/60}:${((millisUntilFinished/1000)%60)/10}${((millisUntilFinished/1000)%60)%10}"
             }
 
             override fun onFinish() {
                 timerText.text = "done!"
             }
         }
-        startTimerButton.setOnClickListener {  }
+        startTimerButton.setOnClickListener { counter.start() }
 
     }
 }
